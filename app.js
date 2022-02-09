165273a74ff6096671ee1006bc323438ad7b90f0
mitaskin <mi.taskin027@gmail.com>
2022-02-09 11:44:36 +0300

    mod

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const http = require('http')
+const fs = require('fs')
 
 const hostname = '127.0.0.1'
 const port = 3000
+
+const indexPage = fs.readFileSync('index.html')
 
 const server = http.createServer((req, res) => {
 
     console.log(req.url)
     res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello Nodejs')
+    
+    res.end(indexPage)
 
 }
 )
